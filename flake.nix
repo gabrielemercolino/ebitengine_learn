93983f3e5941811a6203a252d24558dd52faedd6
Gabriele <gmercolino2003@gmail.com>
2024-12-07 10:00:15 +0100

    refactor: formatted file

--- flake.nix
+++ flake.nix
@@ -3,13 +3,21 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         abstractApp = pkgs.callPackage ./abstract.nix { };
         fractalApp = pkgs.callPackage ./fractal.nix { };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             go
@@ -23,9 +31,7 @@
             pkg-config
           ];
           shellHook = ''
-            export LD_LIBRARY_PATH=${pkgs.wayland}/lib:${
-              pkgs.lib.getLib pkgs.libGL
-            }/lib:$LD_LIBRARY_PATH
+            export LD_LIBRARY_PATH=${pkgs.wayland}/lib:${pkgs.lib.getLib pkgs.libGL}/lib:$LD_LIBRARY_PATH
             echo "Welcome!" | ${pkgs.cowsay}/bin/cowsay | ${pkgs.lolcat}/bin/lolcat
           '';
         };
@@ -44,6 +50,6 @@
             package = self.packages.${system}.fractal;
           };
         };
-      });
+      }
+    );
 }
-
